ea4b469bf0955d420d88f3984409fa0ae0c4b700
rustamse <rustamse@gmail.com>
2016-10-25 22:57:49 +0500

    Добавил тест на проверку иключительной ситуации просьбы отрицательного объема виски

--- test/barmenTests.js
+++ test/barmenTests.js
@@ -2,72 +2,83 @@ import assert from 'assert'
 import {Barman} from '../src/barmen'
 import {Client} from '../src/client'
 
-suite('when client ask 200 grams of whisky', function () {
+suite('barmen pour whisky', function () {
     const drinkName = 'whisky';
-    const clientAskVolume = 200;
 
-    suite('barman has enough', function () {
+    suite('when client ask 200 grams', function () {
+        const clientAskVolume = 200;
 
-        const cupboardStub = {
-            hasDrink: function () {
-                return true;
-            },
-            getDrink: function () {
-                return clientAskVolume;
-            }
-        };
+        suite('barman has enough', function () {
 
-        test('barman pour 200 grams of whisky', function () {
+            const cupboardStub = {
+                hasDrink: function () {
+                    return true;
+                },
+                getDrink: function () {
+                    return clientAskVolume;
+                }
+            };
+
+            test('barman pour 200 grams of whisky', function () {
+                const clientStub = {
+                    isDrunken: function () {
+                        return false;
+                    }
+                };
+
+                var barman = new Barman(cupboardStub);
+
+                var volumeInGlass = barman.pour(drinkName, clientAskVolume, clientStub);
+
+                assert.equal(clientAskVolume, volumeInGlass);
+            });
+
+            test('barman refused because client is drunked', function () {
+                const clientStub = {
+                    isDrunken: function () {
+                        return true;
+                    }
+                };
+
+                var barman = new Barman(cupboardStub);
+
+                var volumeInGlass = barman.pour(drinkName, clientAskVolume, clientStub);
+
+                assert.equal(0, volumeInGlass);
+            });
+        });
+
+        suite('no whisky in cupboard', function () {
+
+            const cupboardStub = {
+                hasDrink: function () {
+                    return false;
+                }
+            };
             const clientStub = {
                 isDrunken: function () {
                     return false;
                 }
             };
 
-            var barman = new Barman(cupboardStub);
+            test('barman confused while pouring', function () {
+                var barman = new Barman(cupboardStub);
 
-            var volumeInGlass = barman.pour(drinkName, clientAskVolume, clientStub);
-
-            assert.equal(clientAskVolume, volumeInGlass);
-        });
-
-        test('barman refused because client is drunked', function () {
-            const clientStub = {
-                isDrunken: function () {
-                    return true;
-                }
-            };
-
-            var barman = new Barman(cupboardStub);
-
-            var volumeInGlass = barman.pour(drinkName, clientAskVolume, clientStub);
-
-            assert.equal(0, volumeInGlass);
+                assert.throws(() => barman.pour(drinkName, clientAskVolume, clientStub), /Not enough whisky/);
+            });
         });
     });
 
-    suite('no whisky in cupboard', function () {
+    suite('when client ask non positive grams', function () {
+        const clientAskVolume = -10;
 
-        const cupboardStub = {
-            hasDrink: function () {
-                return false;
-            }
-        };
-        const clientStub = {
-            isDrunken: function () {
-                return false;
-            }
-        };
+        const cupboardStub = {};
+        const clientStub = {};
 
         test('barman confused while pouring', function () {
-
             var barman = new Barman(cupboardStub);
 
-            assert.throws(() => barman.pour(drinkName, clientAskVolume, clientStub), /Not enough whisky/);
+            assert.throws(() => barman.pour(drinkName, clientAskVolume, clientStub), /Invalid volume of whisky/);
         });
     });
-
-    teardown(function () {
-        console.log('teardown');
-    })
 });
